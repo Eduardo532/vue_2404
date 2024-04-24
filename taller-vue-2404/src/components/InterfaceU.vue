32ba7509bb55652ed06c23b0d83cb94798cbfa80
Eduardo532 <112228065+Eduardo532@users.noreply.github.com>
2024-04-24 15:18:14 -0400

    from

---
+++ taller-vue-2404/src/components/InterfaceU.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="conteiner">
+
+        <form action="">
+            
+            <div id="form">
+                <label for="nombre">Nombre </label>
+                <input type="text" name="nombre" id="nombre">
+            </div>
+            <div id ="form">
+                <label for="apellido">Apellido </label>
+                <input type="text" name="apellido" id="apellido">
+            </div>
+            <div id ="form">
+                <button type="submit">Enviar</button>
+                <button type="reset">Limpiar</button>
+            </div>
+        </form>
+
+
+    <div id="datos">_</div>
+
+        
+    </div>
+</template>
+
+
+
+
+
+
+<script>
+export default {
+  name: 'InterfaceU'
+  
+}
+</script>
+
+<style>
+#conteiner{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 30px;
+}
+div#form{
+    margin-top: 1%;
+}
+
+button{
+    margin-left: 20px;
+}
+
+div#datos{
+    margin-top: 2%
+}
+</style>
