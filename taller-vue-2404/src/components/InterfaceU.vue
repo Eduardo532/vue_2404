0948f38885aca4952275f18d0003f5f6723a8db9
Eduardo532 <112228065+Eduardo532@users.noreply.github.com>
2024-04-24 15:45:35 -0400

    cambios varios

--- taller-vue-2404/src/components/InterfaceU.vue
+++ taller-vue-2404/src/components/InterfaceU.vue
@@ -4,25 +4,66 @@
         <form action="">
             
             <div id="form">
-                <label for="nombre">Nombre </label>
-                <input type="text" name="nombre" id="nombre">
+                <label for="nombre">Nombre: </label>
+                <input type="text" v-model="nombre">
             </div>
             <div id ="form">
                 <label for="apellido">Apellido </label>
                 <input type="text" name="apellido" id="apellido">
             </div>
             <div id ="form">
-                <button type="submit">Enviar</button>
+                <button @click="">Enviar</button>
                 <button type="reset">Limpiar</button>
             </div>
         </form>
 
 
-    <div id="datos">_</div>
+    <div>
+        
+    </div>
 
         
     </div>
 </template>
+
+<script>
+import { split } from 'core-js/fn/symbol'
+import { ref, reactive, computed,watch } from 'vue'
+
+
+const nombres = ref(['Jane Doe'])
+
+  const selecionado = ref('')
+  const prefijo = ref('')
+  const first = ref('')
+  const last = ref('')
+
+  watch(selecionado,(name)=>{
+    ;[last.value,first.value]=name.split(' ')
+  })
+
+  function enviar(){
+    if (hasValidInput()){
+        const fullName = `${last.value} ${first.value}`
+        if(!nombres.includes(fullName)){
+            nombres.push(fullName)
+            first.value = last.value = ''
+        }
+    }
+  }
+
+  function editar(){
+    if(hasValidInput()&& selecionado.value){
+        const i = nombres.indexOf(selecionado.value)
+        nombres[i]=selecionado.value=`${last.value} ${first.value}` 
+    }
+  }
+
+  
+
+
+
+</script>
 
 
 
